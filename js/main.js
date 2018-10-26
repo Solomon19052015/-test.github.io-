71c0629b2c490cf1ee48798ffbe5f75bdb62ce7b
Solomon19052015 <42998762+Solomon19052015@users.noreply.github.com>
2018-10-26 17:32:31 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+function $(el){
+    return document.querySelector(el);
+}
+
+window.onload = function(){
+   $(".menuToggle").AddEventListener("click", clickHeandler);
+   function clickHeandler(){
+    
+   }
+ 
+}
